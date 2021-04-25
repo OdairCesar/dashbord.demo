05bbf1ee20fbc7076e8527344fa0651bcff9889f
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-04-25 08:27:00 -0300

    Eventos para fazer a navegação no site

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Main from '../template/Main'
 
+const headerProps = {
+    icon: 'home',
+    title: 'Inicio',
+    subtitle: 'Segundo Projeto do capítulo de React.'
+}
+
 export default props =>
-    <Main icon="home" title="Início"
-        subtitle="Segundo Projeto do capítulo de React.">
+    <Main {...headerProps}>
         <div className='display-4'>Bem Vindo!</div>
         <hr />
         <p className="mb-0">Sistema para exemplificar a construção
--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,16 +1,20 @@
 import './Nav.css'
 import React from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            {/* Refatorar em casa! */}
+            {/* Refatorar em casa! 
             <Link to="/">
+                <i className="fa fa-home"></i> Inicio
+            </Link>
+            */}
+            <a href="#/">
                 <i className="fa fa-home"></i> Início
-            </Link>
-            <Link to="/users">
+            </a>
+            <a href="#/users">
                 <i className="fa fa-users"></i> Usuários
-            </Link>
+            </a>
         </nav>
     </aside>
